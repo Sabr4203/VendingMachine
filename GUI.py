2a3c0856eadbc714145556ba47b39fcea5484921
Sabr4203 <Sabr4203@colorado.edu>
2019-11-08 16:00:08 -0700

    Connected to Bitcoin Node, Changed Inventory to JSON file, Integrated QR Code, updated how Refill Works

--- GUI.py
+++ GUI.py
@@ -1,73 +1,171 @@
-import Tkinter
+from sys import (
+    version_info,
+)  # Found this fix online for me TKinter works but tkinter does not
 
-inventory = [3,3,3,3]
-inventoryDisplay = []
+if version_info.major == 2:
+    # We are using Python 2.x
+    import Tkinter as tk
+elif version_info.major == 3:
+    # We are using Python 3.x
+    import tkinter as tk
+
+import json  # for reading the stores stock took recomendation of storing items on a text file
+from bitcoinrpc.authproxy import (
+    AuthServiceProxy,
+    JSONRPCException,
+)  # used to send RPC commands to Bitcoin Node
+import qrcode  # to generate a QR
+import time  # to keep track of time
+import os  # currently using this to reset the store to a deafult with a python script
+
+
+# This is for connecting to a bitcoin node running locally,  in the config file need RPC set up
+rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332" % ("User", "Pass"))
+
+
+inventory = []  # used to keep track of inventory display
+
+# intialize the start of buying an item and check for stock
 def BuyItem():
-    qr.grid(row=0,column=4)
+    unconfirmed = float(
+        rpc_connection.getunconfirmedbalance()
+    )  # check our unconfirmed balance incase we have a transaction that has not gone through yet
 
-    
+    with open("Stock.txt", "r") as json_file:  # load in stock from the store
+        stock = json.load(json_file)
+    slot = int(entry1.get())  # identify which slot was used
 
-def Refill():
-    for i in range(len(inventory)):
-        inventory[i]=5
-    print(inventory)
+    if slot > 6:
+        item = "Snacks"  # figure out what kind of item they are ordering by number of slot, i will probably change the json file to just order based on slot not drinks and snacks
+    else:
+        item = "Drinks"
+    for x in stock["%s" % (item)]:  # cycle through either drinks or snacks
+        if int(x["slot"]) == slot:  # search for by item type by its slot
+            supply = int(x["stock"])  # check our supply of item
+            if supply != 0:  # if we have items in stock
+                adress = (
+                    rpc_connection.getnewaddress()
+                )  # get a new bitcoin address from the node
+                # generate a bitcoin URI
+                request = "bitcoin:%s?amount=%s" % (adress, x["cost"])
+                # make a QR CODE
+                qr = qrcode.make(request, version=1)
+
+                qr.save("qr.png")
+                img = tk.PhotoImage(file="qr.png")
+                # add QR to GUI
+                QR_.config(image=img)
+                QR_.photo = img
+                QR_.grid(row=0, column=4)
+                payment_adress.config(text=adress)  # display address
+                M.update()  # update to show qr code
+                # below is a timer to allow upto 60 seconds until payment is dropped
+                start = time.time()
+                elapsed = 0
+                while elapsed < 20:
+                    if unconfirmed < float(
+                        rpc_connection.getunconfirmedbalance()
+                    ):  # check if we got any new transactions since displaying the QR
+                        x["stock"] = (
+                            supply - 1
+                        )  # if we have it do a payment and reduce supply
+                        with open(
+                            "Stock.txt", "w"
+                        ) as outfile:  # updat JSON to represent the store
+                            json.dump(stock, outfile, indent=4, sort_keys=True)
+                        update()
+                        break
+            else:
+                print("Sorry we are out")
+
+
+def Refill():  # calls python script to generate default store
+    os.system("python default.py")
     update()
-def update():
-    a = 0
-    for i in inventoryDisplay:
-        i.config(text=inventory[a])
-        a += 1
-def payment():
-    qr.grid_remove()
-    i = int(entry1.get()) - 1
-    if inventory[i] != 0 :
-        inventory[i] += -1 
-        print(inventory)
-    entry1.delete(0, 'end')
-    update()
 
 
-M=Tkinter.Tk() 
-M.title('Vending Machine')
-Label1 = Tkinter.Label(M, text='Item You want').grid(row=0)
-entry1 = Tkinter.Entry(M)
+def update():  # update the store front to reflect the JSON file
+    with open("Stock.txt") as json_file:
+        slot = 1
+        stock = json.load(json_file)
+        for i in inventory:  # cycle through the inventory dispaly
 
-entry1.grid(row=0,column=1)
-Snack1 = Tkinter.Label(M, text='1: Ice Tea').grid(row=1,column = 0)
+            # find item assoiceated with each slot
+            if slot > 6:
+                item = "Snacks"
+            else:
+                item = "Drinks"
+            for x in stock["%s" % (item)]:
+                if int(x["slot"]) == slot:
 
-snack = Tkinter.Label(M, text= inventory[0])
-snack.grid(row = 1,column = 1)
-inventoryDisplay.append(snack)
+                    label = "%s: %s cost:%s stock:%s" % (
+                        x["slot"],
+                        x["name"],
+                        x["cost"],
+                        x["stock"],
+                    )
+                    i.config(text=label)
+            slot += 1
 
-Snack2 = Tkinter.Label(M, text='2: Coke').grid(row=2,column = 0)
-
-snack = Tkinter.Label(M, text= inventory[1])
-snack.grid(row = 2,column = 1)
-inventoryDisplay.append(snack)
-
-Snack3 = Tkinter.Label(M, text='3: Sprite').grid(row=3,column = 0)
-
-snack = Tkinter.Label(M, text= inventory[2])
-snack.grid(row = 3,column = 1)
-inventoryDisplay.append(snack)
-
-Snack4 = Tkinter.Label(M, text='4: Coffe').grid(row=4,column = 0)
-
-snack = Tkinter.Label(M, text= inventory[3])
-snack.grid(row = 4,column = 1)
-inventoryDisplay.append(snack)
-
-
-img = Tkinter.PhotoImage(file = "Bicoin.png") 
-img1 = img.subsample(2, 2)
-qr = Tkinter.Label(M, image = img1)
+    entry1.delete(0, "end")  # resets the entry bar
+    logo = tk.PhotoImage(file="Bitcoin.png")  # get rid of the QR
+    QR_.config(image=logo)
+    QR_.photo = logo
+    QR_.grid(row=0, column=4)
+    payment_adress.config(text="")
 
 
-buy = Tkinter.Button(M, text='Buy', command=BuyItem)
-buy.grid(row=0,column=2)
-pay = Tkinter.Button(M, text= "Pay" , command=payment)
-pay.grid(row=1,column = 2)
-refill = Tkinter.Button(M,text="Refill", command=Refill).grid(row=0,column=3)
+# initialize GUI
 
+M = tk.Tk()
+M.title("Vending Machine")  # set title
 
+# Setting up ordering bar
+Label1 = tk.Label(M, text="Item You Want").grid(row=0)
+entry1 = tk.Entry(M)  # text input
+entry1.grid(row=0, column=1)  # adding it to the interface
+
+# set up the stock display
+
+with open("Stock.txt") as json_file:
+    stock = json.load(json_file)
+    row_count = 1
+    for d in stock["Drinks"]:  # cylce through all the drinks
+        label = "%s: %s cost:%s stock:%s" % (
+            d["slot"],
+            d["name"],
+            d["cost"],
+            d["stock"],
+        )
+        drink = tk.Label(M, text=label)
+        drink.grid(row=row_count, column=0)
+        inventory.append(drink)
+        row_count += 1
+    row_count = 1
+    for s in stock["Snacks"]:  # cylce through all the snacks
+        label = "%s: %s cost:%s stock:%s" % (
+            s["slot"],
+            s["name"],
+            s["cost"],
+            s["stock"],
+        )
+        snack = tk.Label(M, text=label)
+        snack.grid(row=row_count, column=1)
+        inventory.append(snack)
+        row_count += 1
+
+# set up buttons
+buy = tk.Button(M, text="Buy", command=BuyItem)
+buy.grid(row=0, column=2)
+
+refill = tk.Button(M, text="Refill", command=Refill).grid(row=0, column=3)
+# qr
+img = tk.PhotoImage(file="Bitcoin.png")
+
+QR_ = tk.Label(M, image=img)
+QR_.photo = img
+QR_.grid(row=0, column=4)
+payment_adress = tk.Label(M, text="")
+payment_adress.grid(row=1, column=4)
 M.mainloop()
+
---
+++ default.py
@@ -0,0 +1,81 @@
+import json
+#I use this file to easily reset the json fies
+data = {}
+#First the drinks
+data['Drinks'] = []
+data['Drinks'].append({
+    'name': 'Pepsi',
+    'cost': '.000011',
+    'stock': '10',
+    'slot': '1'
+})
+data['Drinks'].append({
+    'name': 'Mountain Dew',
+    'cost': '.000012',
+    'stock': '10',
+    'slot': '2'
+})
+data['Drinks'].append({
+    'name': 'Root Beer',
+    'cost': '.000013',
+    'stock': '10',
+    'slot': '3'
+})
+data['Drinks'].append({
+    'name': 'Sprite',
+    'cost': '.000014',
+    'stock': '10',
+    'slot': '4'
+})
+data['Drinks'].append({
+    'name': 'Starbucks Vanilla',
+    'cost': '.0000135',
+    'stock': '10',
+    'slot': '5'
+})
+data['Drinks'].append({
+    'name': 'Starbucks Mocha',
+    'cost': '.000016',
+    'stock': '10',
+    'slot': '6'
+})
+data['Snacks'] = []
+data['Snacks'].append({
+    'name': 'Cheetos',
+    'cost': '.000017',
+    'stock': '10',
+    'slot': '7'
+})
+data['Snacks'].append({
+    'name': 'Dorritos',
+    'cost': '.000018',
+    'stock': '10',
+    'slot': '8'
+})
+data['Snacks'].append({
+    'name': 'Poptart',
+    'cost': '.000019',
+    'stock': '10',
+    'slot': '9'
+})
+data['Snacks'].append({
+    'name': 'Big Texas',
+    'cost': '.000020',
+    'stock': '10',
+    'slot': '10'
+})
+data['Snacks'].append({
+    'name': 'Donut',
+    'cost': '.000021',
+    'stock': '10',
+    'slot': '11'
+})
+data['Snacks'].append({
+    'name': 'Lays',
+    'cost': '.000022',
+    'stock': '10',
+    'slot': '12'
+})
+with open('Stock.txt','w') as outfile:
+    json.dump(data, outfile, indent=4,sort_keys=True)
+    
